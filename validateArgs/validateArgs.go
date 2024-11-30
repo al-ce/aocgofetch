6a8e58e8ffeeaa2bdc1d9f9a08dc79ea332891ec
al-ce <aceserani@gmail.com>
2024-11-30 11:57:36 -0600

    refactor(args): implement Error on ArgsState type

--- validateArgs/validateArgs.go
+++ validateArgs/validateArgs.go
@@ -21,13 +21,22 @@ const (
 	DayArgInvalid
 )
 
+func safeIndex(args []string, index int) string {
+	if index >= 0 && index < len(args) {
+		return args[index]
+	}
+	return ""
+}
+
 func (self ArgsState) Error(args []string) error {
+	year, day := safeIndex(args, 0), safeIndex(args, 1)
+
 	ArgsErrType := map[ArgsState]string{
 		BadArgsAmount:  fmt.Sprintf("got %d args, need exactly 2: <day> <year>", len(args)),
-		YearArgNotInt:  fmt.Sprintf("%s: could not convert year argument to int", args[0]),
-		DayArgNotInt:   fmt.Sprintf("%s: could not convert day argument to int", args[1]),
-		DayArgInvalid:  fmt.Sprintf("%s: day must be between 1 and %d", args[1], getMaxAocDay()),
-		YearArgInvalid: fmt.Sprintf("%s: year must be between 2015 and %d", args[0], getMaxAocYear()),
+		YearArgNotInt:  fmt.Sprintf("%s: could not convert year argument to int", year),
+		DayArgNotInt:   fmt.Sprintf("%s: could not convert day argument to int", day),
+		DayArgInvalid:  fmt.Sprintf("%s: day must be between 1 and %d", day, getMaxAocDay()),
+		YearArgInvalid: fmt.Sprintf("%s: year must be between 2015 and %d", year, getMaxAocYear()),
 	}
 	if err, exists := ArgsErrType[self]; exists {
 		return fmt.Errorf(err)
